c536cfa1e3aebdbb9c8f62e11efbf84990a8f36f
Matt Cool <matthewbcool@gmail.com>
2018-06-12 16:00:13 -0500

    added a second cat as specified by new proj req. wet version

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const catImage = document.getElementById('cat-image');
-const counter = document.getElementById('count');
+//TO DO: refactor using DRY principals
 
-catImage.addEventListener('click', function(){
-    number = parseInt(counter.innerText);
-    counter.innerText = number + 1;
-});
+const catImageOne = document.getElementById('cat-image-one');
+const catImageTwo = document.getElementById('cat-image-two');
+const counterOne = document.getElementById('count-one');
+const counterTwo = document.getElementById('count-two');
+
+const clickingFunctionOne = () => {
+    number = parseInt(counterOne.innerText);
+    counterOne.innerText = number + 1;
+}
+
+const clickingFunctionTwo = () => {
+    number = parseInt(counterTwo.innerText);
+    counterTwo.innerText = number + 1;
+}
+
+catImageOne.addEventListener('click', clickingFunctionOne);
+catImageTwo.addEventListener('click', clickingFunctionTwo);
