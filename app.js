da77f0ed4cf822dddf19edaf0f6137e1a4033759
Matt Cool <matthewbcool@gmail.com>
2018-06-16 15:52:49 -0500

    bootstrapped project and changed layout to meet new specs

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
-//TO DO: refactor using DRY principals
 
-const catImageOne = document.getElementById('cat-image-one');
-const catImageTwo = document.getElementById('cat-image-two');
-const counterOne = document.getElementById('count-one');
-const counterTwo = document.getElementById('count-two');
 
-const clickingFunctionOne = () => {
-    number = parseInt(counterOne.innerText);
-    counterOne.innerText = number + 1;
-}
+const catList = [];
 
-const clickingFunctionTwo = () => {
-    number = parseInt(counterTwo.innerText);
-    counterTwo.innerText = number + 1;
-}
-
-catImageOne.addEventListener('click', clickingFunctionOne);
-catImageTwo.addEventListener('click', clickingFunctionTwo);
